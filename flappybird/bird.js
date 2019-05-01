ecc4a059e99ddbbacdef0cbc9e4e7d59e2994907
Bao-Linh Le Mai <mlblinh@apcs.vn>
2019-05-01 21:33:37 +0700

    Bird hits complete

--- flappybird/bird.js
+++ flappybird/bird.js
@@ -10,8 +10,11 @@ class Bird extends Movable{
     }
 
     hits(pipe){
-        if(this.y < pipe.top + pipe.pipeWidth || this.y > pipe.bottom ){
+        if(this.y < pipe.top + pipe.pipeWidth || this.y > pipe.top + pipe.pipeWidth + pipe.holeDistance ){
             if(this.x > pipe.x-pipe.pipeWidth/3 && this.x < pipe.x-pipe.pipeWidth/3 + pipe.pipeLidWidth){
+                console.log(this.y);
+                console.log(pipe.top + pipe.pipeWidth);
+                console.log(pipe.top + pipe.pipeWidth + pipe.holeDistance);
                 this.isOver = true;
                 return true;
             }
@@ -27,7 +30,7 @@ class Bird extends Movable{
 
 
         fill(255,239,213);
-       ellipse(this.x-26, this.y+5, 30, 20);
+        ellipse(this.x-26, this.y+5, 30, 20);
 
         fill(255,0,0);
         ellipse(this.x+20, this.y+10, 40, 15);
